59a30dd37f45fc5f1a3693c8d8018be6316ba5d0
Timo Feindt <t.feindt93@gmail.com>
2022-02-15 15:34:58 +0100

    styled imageslider and finished functionality also arrange work space

--- js/imageSlider.js
+++ js/imageSlider.js
@@ -3,30 +3,24 @@ let index = 0;
 let imgArray = Array.from(document.querySelectorAll('.imageSmall'));
 let rightBtn = document.getElementById('right');
 let leftBtn = document.getElementById('left');
+let activeImage = document.getElementById('image--active') 
 
-console.log(imgArray[2])
 const nextImage = () => {
-    let activeImage = document.getElementById('#active') 
     if(index == (imgArray.length - 1)) {
         index = 0;
         activeImage.innerHTML = imgArray[0].outerHTML;
-        console.log(index)
     } else if(index < imgArray.length) {
         index += 1
         activeImage.innerHTML = imgArray[index].outerHTML;
-        console.log(index)
     }
 }
 const prevImage = () => {
-    let activeImage = document.getElementById('#active') 
     if(index == 0) {
         index = (imgArray.length - 1);
         activeImage.innerHTML = imgArray[index].outerHTML;
-        console.log(index)
     }else if(index <= imgArray.length){
         index += -1
         activeImage.innerHTML = imgArray[index].outerHTML;
-        console.log(index)
     }
 
 }
